8479ae345f4f845b2ee49986e78b07b5d4a05f2c
Arkild <koray.semenuik@gmail.com>
2023-10-16 10:41:18 -0600

    can't test routes in postman without html pages. pushing code to branch with starting routes

--- mixmatch/main_app/urls.py
+++ mixmatch/main_app/urls.py
@@ -5,6 +5,6 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'),
     path('about/', views.about, name ='about'),
-    path('drinks/', views.drinks_index, name='index'),
+    # path('drinks/', views.drinks_index, name='index'),
     # The rest are class based views - I'll do them later
 ]
--- mixmatch/main_app/views.py
+++ mixmatch/main_app/views.py
@@ -2,14 +2,11 @@ from django.shortcuts import render
 
 # Create your views here.
 def home(request):
-    return print('hello home')
-    # return render(request, 'home.html')
+    return render(request, 'home.html')
 
 def about(request):
-    return print('hello about')
-    # return render(request, 'about.html')
+    return render(request, 'about.html')
 
-def drinks_index(request):
-    return print('hello drinks')
+# def drinks_index(request):
     # drinks index is a more complicated route
     # return render(request, 'drinks/index.html', {'drinks': drinks})
