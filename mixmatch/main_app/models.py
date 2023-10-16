6cc06c19b6586bd6348807f54a81e1fc14dbfa99
winstonl <wrlucasas@gmail.com>
2023-10-16 10:09:59 -0700

    Attemping to fetch DEV

--- mixmatch/main_app/models.py
+++ mixmatch/main_app/models.py
@@ -5,4 +5,9 @@ from django.contrib.postgres.fields import ArrayField
 
 class Drinks(models.Model):
     name = models.CharField(max_length=100)
+    # Found a Field for listing items.
+    # https://docs.djangoproject.com/en/4.2/ref/contrib/postgres/fields/
     ingredients = ArrayField(models.Charfield(max_length=100))
+    measurements = ArrayField(models.Charfield(max_length=100))
+    instructions = models.Textfield(max_length=250)
+    
