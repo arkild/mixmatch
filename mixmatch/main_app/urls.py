640c1cb4d7394eaa21f42d8969656b38380c9fe6
Arkild <koray.semenuik@gmail.com>
2023-10-16 13:36:55 -0600

    all drink-related routes completed, need to be tested on front-end

--- mixmatch/main_app/urls.py
+++ mixmatch/main_app/urls.py
@@ -7,10 +7,11 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'),
     path('about/', views.about, name ='about'),
-    path('drinks/', views.drinks_index, name='index'),
-    path('drinks/<int:drink_id>/', views.drink_detail, name='details'),
-    path('drinks/create/', views.DrinkCreate.as_view(), name='drinks_create'),
-    path('drinks/<int:pk>/update', views.DrinkUpdate.as_view(), name='drinks_update'),
-    path('drinks/<int:pk>/delete', views.DrinkDelete.as_view(), name='drinks_delete'),
-
+    # path('drinks/', views.drinks_index, name='index'),
+    # path('drinks/<int:drink_id>/', views.drink_detail, name='details'),
+    # path('drinks/create/', views.DrinkCreate.as_view(), name='drinks_create'),
+    # path('drinks/<int:pk>/update', views.DrinkUpdate.as_view(), name='drinks_update'),
+    # path('drinks/<int:pk>/delete', views.DrinkDelete.as_view(), name='drinks_delete'),
+    
+    # Still need Reviews to be added for routes later
 ]
