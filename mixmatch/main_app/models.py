d1e9a2f6aba9930e9f17ed74adeea9d855ceb7ed
winstonl <wrlucasas@gmail.com>
2023-10-16 09:57:44 -0700

    testing git push, have al ittle model built.

--- mixmatch/main_app/models.py
+++ mixmatch/main_app/models.py
@@ -1,3 +1,8 @@
 from django.db import models
-
+from django.urls import reverse
+from django.contrib.postgres.fields import ArrayField
 # Create your models here.
+
+class Drinks(models.Model):
+    name = models.CharField(max_length=100)
+    ingredients = ArrayField(models.Charfield(max_length=100))
