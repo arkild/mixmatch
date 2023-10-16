97d581b993bd8830e8a61e26e2835ff295387187
Arkild <koray.semenuik@gmail.com>
2023-10-16 10:35:36 -0600

    added URLS and edited views and urls to accomodate basic get requests

--- mixmatch/main_app/views.py
+++ mixmatch/main_app/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
 
 # Create your views here.
+def home(request):
+    return print('hello home')
+    # return render(request, 'home.html')
+
+def about(request):
+    return print('hello about')
+    # return render(request, 'about.html')
+
+def drinks_index(request):
+    return print('hello drinks')
+    # drinks index is a more complicated route
+    # return render(request, 'drinks/index.html', {'drinks': drinks})
