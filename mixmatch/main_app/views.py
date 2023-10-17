191e027bc78e1d48a78d37d0ab8a6db9237678e4
winstonl <wrlucasas@gmail.com>
2023-10-17 16:33:39 -0700

    added review route to super user

--- mixmatch/main_app/views.py
+++ mixmatch/main_app/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 
-from .models import Drink
+from .models import Drink, Review
 from .forms import ReviewForm
 
 # Create your views here.
