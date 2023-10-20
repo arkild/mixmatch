d8f9afa1273bcfe60e7be75435bcc1acb2fd1943
winstonl <wrlucasas@gmail.com>
2023-10-20 16:43:02 -0700

    deleted everything due to git errors going to restart branch

--- mixmatch/main_app/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from .models import Drink, Review, Photo
-# Register your models here.
-admin.site.register(Drink)
-admin.site.register(Review)
-admin.site.register(Photo)
