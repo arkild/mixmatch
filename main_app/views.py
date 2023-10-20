5787ffd2375ed36355c0bbc26bb6968c80507b2e
robin hunter <rbnrggs@yahoo.com>
2023-10-20 12:04:57 -1000

    update image layout

--- main_app/views.py
+++ main_app/views.py
@@ -139,7 +139,7 @@ def signup(request):
 @login_required
 def user_drinks(request):
     drinks = Drink.objects.filter(user=request.user)
-    return render(request, 'user/index.html', { 'drinks': drinks})
+    return render(request, 'user/index.html', { 'drinks': drinks })
 
 # class SearchView(ListView):
 #     # Pulls from the drink model
